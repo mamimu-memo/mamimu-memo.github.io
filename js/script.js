d9a9c4a4777b10b15cf6a963e67f450be04535e0
mamimu-memo <60286812+mamimu-memo@users.noreply.github.com>
2020-03-24 13:59:33 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*$(function() {
+    $("#menu2 ul li a").click(function(){
+        $('body').addClass("fixed")
+    });
+    $(".lightboxOverlay,.lb-close,.lightbox").click(function(){
+        $('body').removeClass("fixed")
+    });
+});*/
